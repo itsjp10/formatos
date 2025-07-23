d6231f33fe8c62848b512fbec91a2371dea35ed8
Jean Pierre <jalvarezidep@gmail.com>
2025-07-23 11:00:46 -0500

    sidebar funcional creada

--- mi-app/src/components/Dashboard.jsx
+++ mi-app/src/components/Dashboard.jsx
@@ -1,8 +1,19 @@
 "use client"
 import { useState, useEffect } from 'react'
+import Sidebar, { SidebarItem } from './Sidebar' // <-- Fix import
+import {
+    LifeBuoy,
+    Receipt,
+    Boxes,
+    Package,
+    UserCircle,
+    BarChart3,
+    Settings,
+    Plus,
+} from "lucide-react"
 
 
-function Dashboard({ user, logout }) {
+function Dashboard({ logout }) {
     const [usuario, setUsuario] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -20,8 +31,21 @@ function Dashboard({ user, logout }) {
 
     return (
         <div>
+            <Sidebar>
+                <SidebarItem icon={<Plus size={20} />} text="Nuevo formato" />
+                <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" active />
+                <SidebarItem icon={<UserCircle size={20} />} text="Users" />
+                <SidebarItem icon={<Boxes size={20} />} text="Inventory" />
+                <SidebarItem icon={<Package size={20} />} text="Orders" />
+                <SidebarItem icon={<Receipt size={20} />} text="Billings" />
+                <hr className="my-3" />
+                <SidebarItem icon={<Settings size={20} />} text="Settings" />
+                <SidebarItem icon={<LifeBuoy size={20} />} text="Help" />
+            </Sidebar>
             <h2>Bienvenido, {usuario.name}</h2>
             <button onClick={logout}>Cerrar sesión</button>
+
+
         </div>
     )
 }
---
+++ mi-app/src/components/Formato.jsx
@@ -0,0 +1,9 @@
+"use client"
+import { useState, useEffect } from 'react'
+
+function Formato(){
+    const [data, setData] = useState(null)
+    return (<h1>This is a formato</h1>)
+}
+
+export default Formato
