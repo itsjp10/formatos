a17ac214ffa6432559536384b3707de17a65e04a
Jean Pierre <jalvarezidep@gmail.com>
2025-08-16 04:05:29 -0500

    los encabezados arreglados en estilos para pdf

--- mi-app/src/components/EncabezadoFormatoPDF.jsx
+++ mi-app/src/components/EncabezadoFormatoPDF.jsx
@@ -1,134 +1,66 @@
-export default function EncabezadoFormato({ tipoFormato, contenidoFormato, hayFilas }) {
+export default function EncabezadoFormato({ tipoFormato, contenidoFormato, hayFilas, hoja }) {
     const titulos = contenidoFormato.titulos
     const columnas = contenidoFormato.columnas
 
     return (
         <div className="w-full relative text-sm">
-            {/* Tabla invisible para igualar estructura de columnas */}
-            <table className="absolute opacity-0 pointer-events-none table-fixed w-full border-collapse">
-                <thead>
-                    <tr>
-                        {columnas.map((col, idx) => {
-                            if (col.fixed) {
-                                return (
-                                    <th
-                                        key={idx}
-                                        rowSpan={3}
-                                        className="border px-2 py-1"
-                                    >
-                                        {col.label}
-                                    </th>
-                                );
-                            } else if (col.subheaders?.length > 0) {
-                                return col.subheaders.map((sub, subIdx) => (
-                                    <th
-                                        key={`${idx}-${subIdx}`}
-                                        colSpan={2}
-                                        className="border px-2 py-1"
-                                    >
-                                        {sub}
-                                    </th>
-                                ));
-                            } else {
-                                return (
-                                    <th
-                                        key={idx}
-                                        colSpan={2}
-                                        rowSpan={2}
-                                        className="border px-2 py-1"
-                                    >
-                                        {col.label}
-                                    </th>
-                                );
-                            }
-                        })}
-                        {/* Columna oculta para simular la celda del botón eliminar */}
-
-                    </tr>
-                </thead>
-            </table>
-
             {/* Contenido visible del encabezado */}
             <div className="border border-black"
                 style={{ width: hayFilas ? 'calc(100% - 40px)' : '100%' }}>
                 {/* Título y datos superiores */}
-                <div className="p-2">
-                    <h1 className='text-[18px] font-bold leading-tight'>{tipoFormato}</h1>
-                    <div className="flex justify-between text-[11px] w-full mt-1 font-semibold">
-                        <div className="flex-1 text-left">
-                            <span>COD. {titulos.cod}</span>
-                        </div>
-                        <div className="flex-1 text-center">
-                            <span>APROBÓ: {titulos.aprobo}</span>
-                        </div>
-                        <div className="flex-1 text-right">
-                            <span>FECHA DE EMISIÓN: {titulos.fechaEmision}</span>
-                        </div>
+                <div className="p-2 relative pr-50">
+                    {/* Logos a la derecha, altura completa del contenedor */}
+                    <div className="absolute inset-y-0 right-15 flex items-center gap-7">
+                        <img
+                            className="h-full w-10 object-contain"
+                            src="/images/vive_rio_img.png"
+                            alt="vive rio"
+                        />
+                        <img
+                            className="h-full w-10 object-contain"
+                            src="/images/magdalena.png"
+                            alt="magdalena"
+                        />
+                    </div>
+
+                    <h1 className="text-[18px] font-bold leading-tight">{tipoFormato}</h1>
+
+                    <div className="flex justify-between text-[10px] w-5/6 mt-1 font-semibold">
+                        <div><span>COD. {titulos.cod}</span></div>
+                        <div><span>APROBÓ: {titulos.aprobo}</span></div>
+                        <div><span>FECHA DE EMISIÓN: {titulos.fechaEmision}</span></div>
                     </div>
                 </div>
+
 
                 {/* Campos tipo formulario */}
-                <div className="grid grid-cols-4 border-t border-b border-black text-[11px]">
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">OBRA:</div>
-                    <div className="col-span-1 border-r border-black">
-                        <input
-                            readOnly
-                            name="obra"
-                            value={titulos.obra || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
+                <div className={`grid ${hoja ? "grid-cols-4" : "grid-cols-3"} border-t border-b border-black text-[11px]`}>
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">OBRA:
+                        <div className="ml-1">{titulos.obra || ''}</div>
                     </div>
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">FECHA:</div>
-                    <div className="col-span-1">
-                        <input
-                            readOnly
-                            name="fecha"
-                            type="date"
-                            value={titulos.fecha || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">FECHA:
+                        <div className="ml-1">{titulos.fecha || ''}</div>
+                    </div>
+                    {hoja && (
+                        <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">HOJA:
+                            <div className="ml-1">1</div>
+                        </div>
+                    )}
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">ELABORADO POR:
+                        <div className="ml-1">{titulos.elaboradoPor || ''}</div>
                     </div>
                 </div>
 
-                <div className="grid grid-cols-4 border-b border-black text-[11px]">
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">TORRE:</div>
-                    <div className="col-span-1 border-r border-black">
-                        <input
-                            readOnly
-                            name="torre"
-                            value={titulos.torre || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
+                <div className="grid grid-cols-3 border-b border-black text-[11px]">
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">TORRE:
+                        <div className="ml-1">{titulos.torre || ''}</div>
                     </div>
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">CONTRATISTA:</div>
-                    <div className="col-span-1">
-                        <input
-                            readOnly
-                            name="contratista"
-                            value={titulos.contratista || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">CONTRATISTA:
+                        <div className="ml-1">{titulos.contratista || ''}</div>
                     </div>
-                </div>
+                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">RESIDENTE DE OBRA:
+                        <div className="ml-1">{titulos.residenteObra || ''}</div>
 
-                <div className="grid grid-cols-4 text-[11px]">
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">ELABORADO POR:</div>
-                    <div className="col-span-1 border-r border-black">
-                        <input
-                            readOnly
-                            name="elaboradoPor"
-                            value={titulos.elaboradoPor || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
-                    </div>
-                    <div className="col-span-1 border-r border-black flex items-center p-1 font-semibold">RESIDENTE DE OBRA:</div>
-                    <div className="col-span-1">
-                        <input
-                            readOnly
-                            name="residenteObra"
-                            value={titulos.residenteObra || ''}
-                            className={`w-full p-1 outline-none `}
-                        />
                     </div>
                 </div>
             </div>
