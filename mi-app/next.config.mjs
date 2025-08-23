9af8bc5a0a8563645f13c316bbfa6510170bdc1e
Jean Pierre <jalvarezidep@gmail.com>
2025-08-23 05:57:17 -0500

    deploy v10

--- mi-app/next.config.mjs
+++ mi-app/next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
-  },
+  experimental: { serverComponentsExternalPackages: ['@sparticuz/chromium','puppeteer-core'] }
 };
 export default nextConfig;
